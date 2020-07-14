59f0aed91919f5ac6eb8beb31db285ab2ffa0ead
Michael Sumner <mdsumner@gmail.com>
2020-07-14 22:17:25 +1000

    use anglr

--- MountStHelens_quadmesh.R
+++ MountStHelens_quadmesh.R
@@ -3,14 +3,9 @@ library(raster)
 dem <- raster::raster("10m_DEM/msh2009dem10")
 img <- raster::brick("WA_Mount St Helens_242552_1998_24000_geo.tif")
 
-
-## crop the dem to just the bit we have image for
-dem <- crop(dem, projectExtent(img, dem))
-
-library(quadmesh)
+library(anglr)
 ## note that this makes a copy of the RGB image on disk in PNG format
-qm <- quadmesh(dem, texture = img)
+mesh <- as.mesh3d(dem, image_texture = img)
 
-library(rgl)
-shade3d(qm)
-bg3d("grey")
+plot3d(mesh)
+rgl::aspect3d("iso")
